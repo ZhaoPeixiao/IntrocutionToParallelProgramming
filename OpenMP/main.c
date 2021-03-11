0fe94a7ced1505ae5ccb5e49485def2364be8963
Peixiao Zhao <u6935249@anu.edu.au>
2021-03-11 21:00:43 +0800

    2021.3.11

--- OpenMP/main.c
+++ OpenMP/main.c
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
 void Hello(void)
 {
 	int my_rank = omp_get_thread_num();
-	int thread_count = omp_get_num_threads;
+	int thread_count = omp_get_num_threads();
 
 	printf("Hello from thread %d of %d\n", my_rank, thread_count);
 }
