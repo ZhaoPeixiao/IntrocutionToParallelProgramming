0f3425a5138f026c7ce638aed907a0b672ac6053
Peixiao Zhao <u6935249@anu.edu.au>
2021-06-13 20:01:54 +0800

    git fixed

---
+++ ch03/ch03/helloMPIIO.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+
+/*
+int main(int argc, char** argv) {
+    int ierr, rank, size;
+    MPI_Offset offset;
+    MPI_File   file;
+    MPI_Status status;
+    const int msgsize = 6;
+    char message[7];
+
+    ierr = MPI_Init(&argc, &argv);
+    ierr |= MPI_Comm_size(MPI_COMM_WORLD, &size);
+    ierr |= MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if ((rank % 2) == 0)
+        strcpy(message, "Hello ");
+    else
+        strcpy(message, "World!");
+
+    printf("Rank %d has message <%s>\n", rank, message);
+
+    offset = (msgsize * rank);
+    MPI_File_open(MPI_COMM_WORLD, "helloMPIIO.txt",
+        MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
+    MPI_File_seek(file, offset, MPI_SEEK_SET);
+    MPI_File_write(file, message, msgsize, MPI_CHAR, &status);
+    MPI_File_close(&file);
+
+    MPI_Finalize();
+    return 0;
+} //main()
+*/
